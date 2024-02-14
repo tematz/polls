c39a442dd35192b753da3b06bb85b6b228083ff5
Matheus <matvno@gmail.com>
2024-02-14 16:45:07 -0300

    feat: setup redis and websocket ranking ordering

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { randomUUID } from "crypto";
+import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -27,6 +29,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                 await prisma.vote.delete({
                     where: { id: userPreviousVoteOnPoll.id }
                 })
+
+                const votes = await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+                    votes: Number(votes)
+                })
             } else if (userPreviousVoteOnPoll) {
                 return reply.status(400).send({ message: 'You have already voted on this poll' })
             }
@@ -47,6 +56,13 @@ export async function voteOnPoll(app: FastifyInstance) {
             data: {
                 sessionId, pollId, pollOptionId
             }
+        })
+
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes)
         })
 
         return reply.status(201).send()
